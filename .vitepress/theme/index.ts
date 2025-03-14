4dcf76a428363781e0d4a5895a5075a859a83af8
Ryan Palma <ryahpalma@gmail.com>
2025-03-14 03:57:46 -0300

    add vercel analytics and remove cache folder

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,8 +2,11 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
 import CustomTheme from './CustomTheme.vue'
+import { inject } from "@vercel/analytics"
+import './style.css'
+
+inject();
 
 export default {
   extends: DefaultTheme,
